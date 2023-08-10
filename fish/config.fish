c0f70f3d427ff327b32c2a30c8be4f15653faf23
ddsteed <ddsteed@gmail.com>
2023-08-10 09:56:45 +0800

    backup mac app config

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
+
+source /Users/fengh/.docker/init-fish.sh || true # Added by Docker Desktop
